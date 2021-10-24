42f1aef2512d47f343189a29e30427411bf37305
Yessine-iut <yessinebenelbey0305@gmail.com>
2021-10-24 19:32:39 +0200

    plats

---
+++ node_modules/vue-azure-maps/types/vue-azure-maps.d.ts
@@ -0,0 +1,18 @@
+import atlas from 'azure-maps-control'
+import { PluginFunction } from 'vue'
+
+export declare class VueAzureMaps {
+  constructor(lib: typeof atlas, options?: VueAzureMapsPluginOptions)
+
+  public readonly key: string
+  public readonly atlas: typeof atlas
+
+  public setSubscriptionKey: (key: string) => void
+
+  static install: PluginFunction<VueAzureMapsPluginOptions>
+  static version: string
+}
+
+export interface VueAzureMapsPluginOptions {
+  key: string
+}
